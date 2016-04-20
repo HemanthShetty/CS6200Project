13a71ba856b272aafdc2184d83fb50d7cddb90da
gbakie <gabriel.bakiewicz@gmail.com>
2016-04-20 15:54:22 -0400

    precision at ranking K; can run for multiple systems

--- Evaluation.py
+++ Evaluation.py
@@ -1,36 +1,41 @@
 
+import getopt
+import sys
 from collections import defaultdict
 
 QUERY_DOCUMENTS_FILE = "../data/cacm.rel"
 
 
-def readQueryDocumentsRanking():
+def readQueryDocumentsRanking(filename):
     query_document = defaultdict(list)
 
-    fp = open(QUERY_DOCUMENTS_FILE, 'r')
+    fp = open(filename, 'r')
     lines = fp.readlines()
 
     for line in lines:
-        q_id,_,doc_id,_ = line.split(' ')
+        q_id,_,doc_id,_ = line.split(' ')[:4]
 
         query_document[int(q_id)].append(doc_id)
+
+    fp.close()
 
     return query_document
 
 def calcQueryPrecision(relevant_docs, docs):
      
-    presicion_docs = []
+    precision_docs = []
     docs_found = 0
+    
     for n,d in enumerate(docs,start=1):
         if d in relevant_docs:
             docs_found += 1
 
         p = float(docs_found) / n
-        presicion_docs.append(p)
+        precision_docs.append(p)
 
     avg_precision = sum(precision_docs) / len(precision_docs)
 
-    return avg_precision
+    return avg_precision, precision_docs
 
 
 def calcMeanAvgPrecision(query_relevant_docs, query_results):
@@ -40,7 +45,7 @@ def calcMeanAvgPrecision(query_relevant_docs, query_results):
     # evaluate for every query and relevant documents
     for q_id,relevant_docs in query_relevant_docs.items():
         query_docs = query_results[q_id]
-        avg_p = calcQueryPrecision(relevant_docs, query_docs) 
+        avg_p,_ = calcQueryPrecision(relevant_docs, query_docs) 
 
         avg_queries_precision.append(avg_p)
 
@@ -48,10 +53,20 @@ def calcMeanAvgPrecision(query_relevant_docs, query_results):
     
     return mean_avg_precision
 
-# TODO
-def calcPrecisionatK(query_relevant_docs, query_results, k):
+
+def calcPrecisionAtK(query_relevant_docs, query_results, k):
+    precision = []
+
+    # evaluate for every query and relevant documents
+    for q_id,relevant_docs in query_relevant_docs.items():
+        query_docs = query_results[q_id]
+        _,p = calcQueryPrecision(relevant_docs, query_docs)
+
+        precision.append(p[k-1])
+
+    mean_precision = sum(precision) / len(precision)
     
-    return 0
+    return mean_precision
 
 
 def calcRR(relevant_docs, docs):
@@ -78,6 +93,38 @@ def calcMeanRR(query_relevant_docs, query_results):
 
     return mean_RR
 
-# test
-#qc = readQueryDocumentsRanking()
-#print qc
+def main():
+    
+    try:
+        opts,args = getopt.getopt(sys.argv[1:], '', ['sys='])
+    except getopt.GetoptError, err:
+        print str(err)
+        sys.exit(-1)
+    
+    opts = {x[0]:x[1] for x in opts}
+    sys_id = int(opts['--sys'])
+
+    if sys_id not in range(1,7) or sys_id == 3:
+        print "System number has to be from 1 to 7. 3 is Lucene"
+        sys.exit(-1)
+
+    
+    sys_filename = "model%d_queries_results.txt" % sys_id 
+
+    sys_results = readQueryDocumentsRanking(sys_filename)
+    eval_results = readQueryDocumentsRanking(QUERY_DOCUMENTS_FILE)
+
+    mean_avg_prec = calcMeanAvgPrecision(eval_results, sys_results)
+    mRR = calcMeanRR(eval_results, sys_results)
+    pAt5 = calcPrecisionAtK(eval_results, sys_results, k=5)
+    pAt20 = calcPrecisionAtK(eval_results, sys_results, k=20)
+
+    print "Results for system %d\n" % sys_id 
+
+    print "MAP: \t%f" % mean_avg_prec 
+    print "MRR: \t%f" % mRR 
+    print "P@5: \t%f" % pAt5 
+    print "P@20: \t%f" % pAt20 
+
+if __name__ == '__main__':
+    main()
